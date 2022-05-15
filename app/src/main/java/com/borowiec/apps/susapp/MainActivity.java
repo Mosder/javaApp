a97290629c5e6b1837073d904ef9a2b975363c5e
Mosder <xmosder12@gmail.com>
2022-05-16 00:25:04 +0200

    camerus dsus saveus notus workus

--- app/src/main/java/com/borowiec/apps/susapp/MainActivity.java
+++ app/src/main/java/com/borowiec/apps/susapp/MainActivity.java
@@ -1,20 +1,32 @@
 package com.borowiec.apps.susapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -34,42 +46,21 @@ public class MainActivity extends AppCompatActivity {
         checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, 100);
 
         for (Map.Entry<String, LinearLayout> entry : buttons.entrySet()) {
-            if (entry.getKey().equals("camera")) {
-                entry.getValue().setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(MainActivity.this, CameraActivity.class);
-                        startActivity(intent);
-                    }
-                });
-            }
-            else if (entry.getKey().equals("albums")) {
-                entry.getValue().setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(MainActivity.this, AlbumsActivity.class);
-                        startActivity(intent);
-                    }
-                });
-            }
-            else if (entry.getKey().equals("albumsNew")) {
-                entry.getValue().setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(MainActivity.this, AlbumsNewActivity.class);
-                        startActivity(intent);
-                    }
-                });
-            }
-            else if (entry.getKey().equals("notes")) {
-                entry.getValue().setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(MainActivity.this, NotesActivity.class);
-                        startActivity(intent);
-                    }
-                });
-            }
+            entry.getValue().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Map<String, Class> activities = new HashMap<>();
+                    activities.put("albums", AlbumsActivity.class);
+                    activities.put("albumsNew", AlbumsNewActivity.class);
+                    activities.put("notes", NotesActivity.class);
+
+                    String key = entry.getKey();
+                    if (key.equals("camera"))
+                        checkPermission(Manifest.permission.CAMERA, 101);
+                    else
+                        startActivity(new Intent(MainActivity.this, activities.get(key)));
+                }
+            });
         }
     }
 
@@ -78,8 +69,12 @@ public class MainActivity extends AppCompatActivity {
         if (ContextCompat.checkSelfPermission(MainActivity.this, permission) == PackageManager.PERMISSION_DENIED) {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{permission}, requestCode);
         } else {
-            Toast.makeText(MainActivity.this, "Permission already granted", Toast.LENGTH_SHORT).show();
-            createFolders();
+            if (requestCode == 100) {
+                Toast.makeText(MainActivity.this, "Permission already granted", Toast.LENGTH_SHORT).show();
+                createFolders();
+            }
+            else if (requestCode == 101)
+                openCamera();
         }
     }
 
@@ -97,19 +92,60 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case 100:
-                if (grantResults.length > 0 &&
-                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    createFolders();
-                } else {
-                    //nie
-                }
-                break;
-            case 101 :
-
-                break;
+        if (grantResults.length > 0 &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (requestCode == 100)
+                createFolders();
+            else if (requestCode == 101)
+                openCamera();
         }
+    }
 
+    public void openCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 200); // 200 - stała wartość, która później posłuży do identyfikacji tej akcji
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 200) {
+            if (resultCode == RESULT_OK) {
+                Bundle extras = data.getExtras();
+                Bitmap bitmap = (Bitmap) extras.get("data");
+
+                AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                alert.setTitle("Save where?");
+                String[] buttons = {"ludzie", "miejsca", "rzeczy"};
+                alert.setItems(buttons, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int album) {
+                        savePhoto(bitmap, String.valueOf(buttons[album]));
+                    }
+                });
+                alert.show();
+            }
+        }
+    }
+
+    public void savePhoto(Bitmap bitmap, String album) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream); // kompresja, typ pliku jpg, png
+        byte[] byteArray = stream.toByteArray();
+
+        SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        String name = df.format(new Date()) + ".jpg";
+
+        FileOutputStream fs = null;
+        try {
+            File file = new File(String.format("%s/MaciejBorowiec/%s",
+                    Environment.DIRECTORY_PICTURES, album), name);
+            fs = new FileOutputStream(file);
+            fs.write(byteArray);
+            fs.close();
+        } catch (IOException e) {
+            Log.d("file", "Exception: " + e);
+        }
     }
 }
